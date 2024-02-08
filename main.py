4f678700e5dfe565f528685b46a943085aeaed32
sreekalap <90302933+sreekalap@users.noreply.github.com>
2024-02-08 10:46:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# Main Program to run all the Programs
 import json
 import  divisible_by_5_7
 import interleaves_2_strings
